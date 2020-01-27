03277eeb49632b66bb349a16543b0cf18684e0bd
ma0c <ma0@contraslash.com>
2020-01-26 22:28:24 -0500

    slides until spectogram

--- components/simple_component.js
+++ components/simple_component.js
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect }  from 'react';
+import React, { useEffect, useState, useCallback }  from 'react';
 import ReactDOM from 'react-dom'
+import {useDropzone} from 'react-dropzone'
 import WaveSurfer from 'wavesurfer.js';
 
 
@@ -19,7 +20,7 @@ export class SimpleWave extends React.Component {
       progressColor: 'purple',
       mediaControls: true,
       backend: 'MediaElement'
-    })
+    });
     this.wavesurfer.load(this.props.src)
   }
   componentWillUnmount() {
@@ -27,13 +28,69 @@ export class SimpleWave extends React.Component {
   }
   render() {
     return (
-      <div className='waveform' style={{width: "100%"}}>
-        <div className='wave'></div>
-      </div>
+        <div className='waveform' style={{width: "100%"}}>
+          <div className='wave'></div>
+        </div>
     )
   }
 }
 
 SimpleWave.defaultProps = {
   src: ""
-}
+};
+
+
+export const MyDropZone = () => {
+  const [files, setFiles] = useState([]);
+
+  const audios = files.map(file => (
+    <div key={file.name}>
+        <SimpleWave
+          src={file.src}
+        />
+    </div>
+  ));
+
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        src: URL.createObjectURL(file)
+      })));
+      reader.readAsArrayBuffer(file);
+
+
+    })
+  }, []);
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({accept: 'audio/wav', onDrop});
+  console.log(isDragActive);
+
+  useEffect(() => () => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, [files]);
+
+  return (
+      <div>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {
+            isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>Drag 'n' drop some files here, or click to select files</p>
+          }
+        </div>
+        <div>
+          {audios}
+        </div>
+      </div>
+  )
+};
--- containers/layout.js
+++ containers/layout.js
@@ -17,6 +17,14 @@ export const HorizontalAlign = styled.div`
     justify-content: center;
 `;
 
+export const JustifyAlign = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    text-align: justify;
+`;
+
 export const Divider = ( { width = "10vw", height = "10vh" }) => {
     console.log(height);
     console.log(width);
